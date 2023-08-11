2203f44ccf6ca2db02f3361ccad51cf3451b1020
revione <revione@outlook.com>
2023-08-11 21:06:17 +0200

    home update

--- src/Router/routes/Home/index.tsx
+++ src/Router/routes/Home/index.tsx
@@ -1,10 +1,10 @@
-import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { motion } from "framer-motion"
 
 const Home = () => {
   return (
     <motion.div
-      className="flex flex-col gap-2"
+      className="flex flex-col min-h-[100vh] w-full justify-between mx-5 h-20"
       initial={{
         opacity: 0
       }}
@@ -16,17 +16,22 @@ const Home = () => {
         }
       }}
     >
-      <div>
-        <NavLink
-          to="/auth"
-          className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "active" : ""
-          }
-        >
-          In
-        </NavLink>
+      <header className="flex w-full justify-between mx-5 h-20 items-center">
+        <div>Rev Trade</div>
+      </header>
+
+      <div className="flex justify-center ">
+        <div>
+          Let's <Link to="/auth">go in!</Link>
+        </div>
       </div>
-      <div>Rev Trade</div>
+
+      <footer className="flex flex-col gap-4 h-60 items-center justify-end pb-10">
+        <div>
+          <a>terms and service</a>
+        </div>
+        <div>Todos los derechos Reservdos - RevDev</div>
+      </footer>
     </motion.div>
   )
 }
