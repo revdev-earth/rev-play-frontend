0f4f771d4c2f321b2b165a19aa1c6d7ba9cbb10a
revione <revione@outlook.com>
2023-08-11 02:29:13 +0200

    nick or email

--- src/Router/routes/Auth/index.tsx
+++ src/Router/routes/Auth/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react"
+import React, { useCallback, useState } from "react"
 import { useDispatch } from "+redux"
 import { useNavigate } from "react-router-dom"
 import { set_session_token } from "+redux/slices/access"
@@ -6,6 +6,7 @@ import { motion } from "framer-motion"
 import { debounce } from "lodash"
 
 const initial_state = {
+  nick_or_email: "",
   nick: "",
   email: "",
   password: "",
@@ -16,20 +17,24 @@ const labelStyle = "flex flex-col gap-4 relative group cursor-default"
 const textStyle =
   "absolute text-sm opacity-50 z-10 group-hover:-top-5 group-hover:left-0 transition-all cursor-default"
 const inputStyle =
-  "border px-2 py-1 transition-all duration-300 focus:outline-none focus:border-blue-500 cursor-default"
+  "border border-solid rounded px-2 py-1 transition-all duration-300 focus:outline-none focus:border-blue-500 cursor-default"
 
 const Auth = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
-  const [loginData, setLoginData] = useState(initial_state)
+  const [auth, setAuth] = useState(initial_state)
   const [tokenLogin, setTokenLogin] = useState<string>()
 
   const [vipCodeActive, setVipCodeActive] = useState(false)
   const [userExist, setUserExist] = useState(false)
-  const [showPassword, setShowPassword] = useState(false)
 
-  const nickCheck = useCallback(async (nick: string) => {
+  const [buttonLoading, setButtonLoading] = useState(false)
+
+  const [isUserChecked, setIsUserChecked] = useState(false)
+  const [isNick, setIsNick] = useState(false)
+
+  const checkUser = useCallback(async (nick: string) => {
     try {
       const response = await fetch(
         `${import.meta.env.VITE_BACK_URL}/auth/checkUser`,
@@ -39,37 +44,17 @@ const Auth = () => {
           headers: { "Content-Type": "application/json" }
         }
       )
-      const res = (await response.json()) as { token: string }
-
-      if (response.ok) {
-        console.log({ res })
-        setUserExist(true)
-      } else {
-        setUserExist(false)
-        console.error("Check user", res)
-      }
-      setShowPassword(true)
+      // const res = (await response.json()) as { message: string }
+      setUserExist(response.ok)
+      setIsUserChecked(true)
+      setButtonLoading(false)
     } catch (error) {
-      console.error("Error during login:", error)
+      console.log("Error during login:", error)
     }
   }, [])
 
-  useEffect(() => {
-    nickCheck(loginData.nick)
-  }, [])
-
-  const debouncedNickCheck = debounce(nickCheck, 400, {
-    leading: false,
-    trailing: true
-  })
-
-  const nickChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    debouncedNickCheck(e.target.value)
-    inputChange(e)
-  }
-
   const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLoginData((prevState) => ({
+    setAuth((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value
     }))
@@ -82,8 +67,8 @@ const Auth = () => {
         {
           method: "POST",
           body: JSON.stringify({
-            nick: loginData.nick,
-            password: loginData.password
+            nick: auth.nick,
+            password: auth.password
           }),
           headers: { "Content-Type": "application/json" }
         }
@@ -95,10 +80,10 @@ const Auth = () => {
         setVipCodeActive(true)
         setTokenLogin(res.token)
       } else {
-        console.error("Login failed", res)
+        console.log("Login failed", res)
       }
     } catch (error) {
-      console.error("Error during login:", error)
+      console.log("Error during login:", error)
     }
   }
 
@@ -109,9 +94,9 @@ const Auth = () => {
         {
           method: "POST",
           body: JSON.stringify({
-            nick: loginData.nick,
-            email: loginData.email,
-            password: loginData.password
+            nick: auth.nick,
+            email: auth.email,
+            password: auth.password
           }),
           headers: { "Content-Type": "application/json" }
         }
@@ -123,10 +108,10 @@ const Auth = () => {
         setVipCodeActive(true)
         setTokenLogin(res.token)
       } else {
-        console.error("Login failed", res)
+        console.log("Login failed", res)
       }
     } catch (error) {
-      console.error("Error during login:", error)
+      console.log("Error during login:", error)
     }
   }
 
@@ -136,7 +121,7 @@ const Auth = () => {
         `${import.meta.env.VITE_BACK_URL}/auth/vipCode`,
         {
           method: "POST",
-          body: JSON.stringify({ vipCode: loginData.vipCode }),
+          body: JSON.stringify({ vipCode: auth.vipCode }),
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${tokenLogin}`
@@ -153,10 +138,10 @@ const Auth = () => {
         dispatch(set_session_token({ sessionToken: token, sessionExpiresIn }))
         navigate("/access")
       } else {
-        console.error("vip code failed", await response.json())
+        console.log("vip code failed", await response.json())
       }
     } catch (error) {
-      console.error("Error during login:", error)
+      console.log("Error during login:", error)
     }
   }
 
@@ -169,6 +154,17 @@ const Auth = () => {
 
   const textStyleAction = (length: number) =>
     textStyle + (length > 0 ? " -top-5 left-0" : " top-1 left-3")
+
+  const nickOrEmail = () => {
+    setButtonLoading(true)
+    const isEmail = auth.nick_or_email.includes("@")
+    setIsNick(!isEmail)
+    setAuth((prevState) => ({
+      ...prevState,
+      [isEmail ? "email" : "nick"]: auth.nick_or_email
+    }))
+    checkUser(auth.nick_or_email)
+  }
 
   return (
     <motion.div
@@ -183,18 +179,7 @@ const Auth = () => {
         }
       }}
     >
-      <label htmlFor="nick" className={labelStyle}>
-        <div className={textStyleAction(loginData.nick.length)}>nick</div>
-        <input
-          name="nick"
-          type="text"
-          onChange={nickChange}
-          value={loginData.nick}
-          className={inputStyle}
-        />
-      </label>
-
-      {!userExist && (
+      {!isUserChecked && (
         <motion.label
           initial={{
             opacity: 0,
@@ -207,93 +192,155 @@ const Auth = () => {
               duration: 0.5
             }
           }}
-          htmlFor="email"
-          className={`${labelStyle}`}
+          htmlFor="nick_or_email"
+          className={labelStyle}
         >
-          <div className={textStyleAction(loginData.password.length)}>
-            Email
+          <div className={textStyleAction(auth.nick_or_email.length)}>
+            nick or email
           </div>
-          <input
-            name="email"
-            type="email"
-            onChange={inputChange}
-            value={loginData.email}
-            className={inputStyle}
-          />
+          <div className="flex gap-3">
+            <input
+              name="nick_or_email"
+              type="text"
+              onChange={inputChange}
+              value={auth.nick_or_email}
+              className={inputStyle}
+              disabled={isUserChecked}
+            />
+            <button className="px-2 py-0" onClick={nickOrEmail}>
+              {!buttonLoading ? "✓" : "🖕🏽"}
+            </button>
+          </div>
         </motion.label>
       )}
 
-      {showPassword && (
-        <motion.label
-          initial={{
-            opacity: 0,
-            height: 0
-          }}
-          animate={{
-            opacity: 1,
-            height: "auto",
-            transition: {
-              duration: 0.5
-            }
-          }}
-          htmlFor="password"
-          className={`${labelStyle}`}
-        >
-          <div className={textStyleAction(loginData.password.length)}>
-            ••••••••
-          </div>
-          <input
-            name="password"
-            type="password"
-            onChange={inputChange}
-            value={loginData.password}
-            className={inputStyle}
-          />
-        </motion.label>
-      )}
+      {isUserChecked && (
+        <>
+          {isNick && (
+            <motion.label
+              initial={{
+                opacity: 0,
+                height: 0
+              }}
+              animate={{
+                opacity: 1,
+                height: "auto",
+                transition: {
+                  duration: 0.5
+                }
+              }}
+              htmlFor="nick"
+              className={labelStyle}
+            >
+              <div className={textStyleAction(auth.nick.length)}>nick</div>
+              <input
+                name="nick"
+                type="text"
+                onChange={inputChange}
+                value={auth.nick}
+                className={inputStyle}
+              />
+            </motion.label>
+          )}
 
-      {vipCodeActive && (
-        <motion.label
-          initial={{
-            opacity: 0,
-            height: 0
-          }}
-          animate={{
-            opacity: 1,
-            height: "auto",
-            transition: {
-              duration: 0.5
-            }
-          }}
-          htmlFor="vipCode"
-          className={`${labelStyle}`}
-        >
-          <div className={textStyleAction(loginData.vipCode.length)}>
-            Vip Code
-          </div>
-          <input
-            name="vipCode"
-            type="password"
-            onChange={inputChange}
-            value={loginData.vipCode}
-            className={inputStyle}
-          />
-        </motion.label>
-      )}
+          {!isNick && (
+            <motion.label
+              initial={{
+                opacity: 0,
+                height: 0
+              }}
+              animate={{
+                opacity: 1,
+                height: "auto",
+                transition: {
+                  duration: 0.5
+                }
+              }}
+              htmlFor="email"
+              className={`${labelStyle}`}
+            >
+              <div className={textStyleAction(auth.password.length)}>Email</div>
+              <input
+                name="email"
+                type="email"
+                onChange={inputChange}
+                value={auth.email}
+                className={inputStyle}
+              />
+            </motion.label>
+          )}
 
-      <button
-        type="button"
-        onClick={inClick}
-        className="
+          <motion.label
+            initial={{
+              opacity: 0,
+              height: 0
+            }}
+            animate={{
+              opacity: 1,
+              height: "auto",
+              transition: {
+                duration: 0.5
+              }
+            }}
+            htmlFor="password"
+            className={`${labelStyle}`}
+          >
+            <div className={textStyleAction(auth.password.length)}>
+              ••••••••
+            </div>
+            <input
+              name="password"
+              type="password"
+              onChange={inputChange}
+              value={auth.password}
+              className={inputStyle}
+            />
+          </motion.label>
+
+          {vipCodeActive && (
+            <motion.label
+              initial={{
+                opacity: 0,
+                height: 0
+              }}
+              animate={{
+                opacity: 1,
+                height: "auto",
+                transition: {
+                  duration: 0.5
+                }
+              }}
+              htmlFor="vipCode"
+              className={`${labelStyle}`}
+            >
+              <div className={textStyleAction(auth.vipCode.length)}>
+                Vip Code
+              </div>
+              <input
+                name="vipCode"
+                type="password"
+                onChange={inputChange}
+                value={auth.vipCode}
+                className={inputStyle}
+              />
+            </motion.label>
+          )}
+
+          <button
+            type="button"
+            onClick={inClick}
+            className="
           relative py-2 px-4 opacity-50 border-2 rounded
           bg-blue-500 text-white  
           hover:bg-blue-300 hover:text-black hover:border-yellow-700 
           hover:font-bold focus:outline-none focus:ring focus:ring-blue-300 
           transition-all
         "
-      >
-        in
-      </button>
+          >
+            {!buttonLoading ? "in" : "...🫷🏽😃🫸🏽..."}
+          </button>
+        </>
+      )}
     </motion.div>
   )
 }
