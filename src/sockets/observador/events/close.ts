5cca1cd7cd3f47f742ada36046088070efff2fba
revione <revione@outlook.com>
2023-08-11 12:19:52 +0200

    state when app can not connect

--- src/sockets/observador/events/close.ts
+++ src/sockets/observador/events/close.ts
@@ -4,5 +4,9 @@ export const close = (event: Event) => {
   if (state.logs.show_error_logs) {
     console.log(":: socket ticks closed : ", event)
     console.log(JSON.stringify(event, null, 2))
+
+    setTimeout(() => {
+      location.reload()
+    }, 1000)
   }
 }
--- src/sockets/observador/events/error.ts
+++ src/sockets/observador/events/error.ts
@@ -4,5 +4,9 @@ export const error = (event: Event) => {
   if (state.logs.show_error_logs) {
     console.log(":: socket ticks tiene un error : ", event)
     console.log(JSON.stringify(event, null, 2))
+
+    setTimeout(() => {
+      location.reload()
+    }, 1000)
   }
 }
