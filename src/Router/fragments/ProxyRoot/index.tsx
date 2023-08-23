20b2b0ac82a0f5d7044e8695c908a147f861234b
revione <revione@outlook.com>
2023-08-23 08:37:20 +0200

    Proxy Root

--- src/Router/fragments/ProxyRoot/index.tsx
+++ src/Router/fragments/ProxyRoot/index.tsx
@@ -1,42 +1,51 @@
 import { useSelector } from "+redux"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Outlet, useNavigate } from "react-router-dom"
 import { Navigate } from "react-router-dom"
 
-export const ProxyRoot = () => {
+export default () => {
   const navigate = useNavigate()
+  const [isSessionValid, setIsSessionValid] = useState(true)
 
   const { sessionToken, sessionExpiresIn } = useSelector((s) => s.access)
 
+  const validateSession = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BACK_URL}/auth/checkAccess`,
+        { headers: { Authorization: `Bearer ${sessionToken}` } }
+      )
+      if (!response.ok) setIsSessionValid(false)
+    } catch (error) {
+      console.log("Error during session validation:", error)
+    }
+  }
+
+  const handleSessionExpiration = () => {
+    setIsSessionValid(false)
+    localStorage.removeItem("state")
+    navigate("/", { replace: true })
+  }
+
+  const tiempoRestante = (sessionTimeout: number) => {
+    const minutes = Math.floor(sessionTimeout / 60000) // 60000 ms = 1 min
+    const seconds = Math.floor((sessionTimeout % 60000) / 1000)
+    console.log(`Tiempo restante: ${minutes}:${seconds}`)
+  }
+
   useEffect(() => {
-    const checkAccess = async () => {
-      try {
-        const response = await fetch(
-          `${import.meta.env.VITE_BACK_URL}/auth/checkAccess`,
-          {
-            headers: { Authorization: `Bearer ${sessionToken}` }
-          }
-        )
+    validateSession()
 
-        if (!response.ok) {
-          localStorage.setItem("state", JSON.stringify({}))
-          navigate("/")
-        }
-      } catch (error) {
-        console.log("Error during login:", error)
-      }
+    const sessionTimeout = sessionExpiresIn - Date.now()
+    tiempoRestante(sessionTimeout)
+
+    if (sessionTimeout > 0) {
+      const timeoutId = setTimeout(validateSession, sessionTimeout)
+      return () => clearTimeout(timeoutId)
+    } else {
+      handleSessionExpiration()
     }
+  }, [sessionToken, sessionExpiresIn, navigate])
 
-    checkAccess()
-    const Timeout = setTimeout(() => {
-      checkAccess()
-    }, sessionExpiresIn - Date.now())
-    return () => {
-      clearTimeout(Timeout)
-    }
-  }, [])
-
-  if (!sessionToken) return <Navigate to="/" replace={true} />
-
-  return <Outlet />
+  return !isSessionValid ? <Navigate to="/" replace={true} /> : <Outlet />
 }
