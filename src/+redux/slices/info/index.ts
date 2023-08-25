142a91289eb5e74d2d1afb11797d1a2591b73642
revione <revione@outlook.com>
2023-08-25 06:14:40 +0200

    clean button

--- src/+redux/slices/info/index.ts
+++ src/+redux/slices/info/index.ts
@@ -9,10 +9,12 @@ const info_slice = createSlice({
     rewrite_info: (state, action) => ({
       ...state,
       ...action.payload
-    })
+    }),
+
+    clean_info: () => initial.info
   }
 })
 
-export const { rewrite_info } = info_slice.actions
+export const { rewrite_info, clean_info } = info_slice.actions
 
 export const reducer = info_slice.reducer
--- src/Router/routes/Trade/fragments/ButtonPanel/index.tsx
+++ src/Router/routes/Trade/fragments/ButtonPanel/index.tsx
@@ -1,5 +1,6 @@
 import { stop, pausa, play } from "+local"
 import { useDispatch } from "+redux"
+import { clean_info } from "+redux/slices/info"
 import { clean_purchases } from "+redux/slices/purchases"
 
 export default () => {
@@ -7,6 +8,7 @@ export default () => {
 
   const clean = () => {
     dispatch(clean_purchases())
+    dispatch(clean_info())
   }
 
   return (
@@ -14,9 +16,7 @@ export default () => {
       <button onClick={play}>play</button>
       <button onClick={pausa}>pausa</button>
       <button onClick={stop}>stop</button>
-      <button onClick={clean} disabled>
-        clean
-      </button>
+      <button onClick={clean}>clean</button>
     </div>
   )
 }
