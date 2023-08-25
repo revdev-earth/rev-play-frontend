bae94e68748a71b8bb34219379d06e7bb5661d53
revione <revione@outlook.com>
2023-08-25 06:14:57 +0200

    remove comments

--- src/sockets/buyer/index.ts
+++ src/sockets/buyer/index.ts
@@ -1,37 +1,20 @@
-// Importing necessary modules and functions
 import { message } from "./events/message"
 import { open } from "./events/open"
 import { close } from "./events/close"
 import { error } from "./events/error"
-import { state } from "+local" // Assuming this module is defined elsewhere
+import { state } from "+local"
 
-// Authentication variables
-const app_id = import.meta.env.VITE_APP_ID // App ID for authentication
-const url = import.meta.env.VITE_BRO // Base URL for WebSocket connection
+const app_id = import.meta.env.VITE_APP_ID
+const url = import.meta.env.VITE_BRO
 
-// URI for connecting to the WebSocket server
-const uri = `${url}${app_id}` // Constructing the complete WebSocket URI
+const uri = `${url}${app_id}`
 
-/**
- * Function to establish a WebSocket connection
- */
-const connect = () => {
+export default () => {
   console.log(" :: socket buyer launched")
-
-  // Creating a new WebSocket instance
   const ws = new WebSocket(uri)
-
-  // Assigning event handlers for WebSocket events
-  ws.onopen = open // Event handler for when the connection is opened
-  ws.onclose = close // Event handler for when the connection is closed
-  ws.onerror = error // Event handler for WebSocket errors
-  ws.onmessage = message // Event handler for incoming messages
-
-  // Storing the WebSocket instance in the state for further reference
-  state.WebSockets.comprador = ws // Assuming "state.WebSockets.comprador" is used elsewhere
+  ws.onopen = open
+  ws.onclose = close
+  ws.onerror = error
+  ws.onmessage = message
+  state.WebSockets.comprador = ws
 }
-
-/**
- * Default function to initiate the WebSocket connection
- */
-export default connect
